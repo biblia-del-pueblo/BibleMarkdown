167d0036234921bc7d622c22490b80cbe75de124
Simon Jakob Egli <simon.jakob.egli@gmail.com>
2025-09-03 15:55:56 +0200

    Fix in Version

--- PushToNuget.bat
+++ PushToNuget.bat
@@ -1,6 +1,9 @@
 SET /p ApiKey=<NugetApiKey.txt
 
-cd nupkg
+cd BibleMarkdown
+dotnet pack
+
+cd ..\nupkg
 
 for /r %%i in (*.nupkg) do (
     dotnet nuget push %%i --api-key %ApiKey% -s https://api.nuget.org/v3/index.json --skip-duplicate
